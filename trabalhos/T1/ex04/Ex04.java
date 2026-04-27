23a91bca223a4dea25bb2813bb0a11a06327fbf8
Marco Antonio Nitsche <marcoantonionitsche@gmail.com>
2026-04-26 21:27:24 -0300

    fluxogramas ate ex07

--- trabalhos/T1/ex04/Ex04.java
+++ trabalhos/T1/ex04/Ex04.java
@@ -19,57 +19,60 @@ public class Ex04 {
         
         valor = lerInteiro("valor: ");
 
-        notas100 = valor / 100;
-        valor = valor % 100;
+        if (valor >= 1 && valor <= 600) {
 
-        notas50 = valor / 50;
-        valor = valor % 50;
+            notas100 = valor / 100;
+            valor = valor % 100;
 
-        notas10 = valor / 10;
-        valor = valor % 10;
+            notas50 = valor / 50;
+            valor = valor % 50;
 
-        notas5 = valor / 5;
-        valor = valor % 5;
+            notas10 = valor / 10;
+            valor = valor % 10;
 
-        notas1 = valor;
+            notas5 = valor / 5;
+            valor = valor % 5;
 
-        if (notas100 > 0) {
-            if (notas100 == 1) {    
-                System.out.println("1 nota de R$ 100");
-            } else {
-                System.out.println(notas100 + " notas de R$ 100");
+            notas1 = valor;
+
+            if (notas100 > 0) {
+                if (notas100 == 1) {    
+                    System.out.println("1 nota de R$ 100");
+                } else {
+                    System.out.println(notas100 + " notas de R$ 100");
+                }
             }
-        }
 
-        if (notas50 > 0) {
-            if (notas50 == 1) {    
-                System.out.println("1 nota de R$ 50");
-            } else {
-                System.out.println(notas50 + " notas de R$ 50");
+            if (notas50 > 0) {
+                if (notas50 == 1) {    
+                    System.out.println("1 nota de R$ 50");
+                } else {
+                    System.out.println(notas50 + " notas de R$ 50");
+                }
             }
-        }
 
-        if (notas10 > 0) {
-            if (notas10 == 1) {    
-                System.out.println("1 nota de R$ 10");
-            } else {
-                System.out.println(notas10 + " notas de R$ 10");
+            if (notas10 > 0) {
+                if (notas10 == 1) {    
+                    System.out.println("1 nota de R$ 10");
+                } else {
+                    System.out.println(notas10 + " notas de R$ 10");
+                }
             }
-        }
 
-        if (notas5 > 0) {
-            if (notas5 == 1) {    
-                System.out.println("1 nota de R$ 5");
-            } else {
-                System.out.println(notas5 + " notas de R$ 5");
+            if (notas5 > 0) {
+                if (notas5 == 1) {    
+                    System.out.println("1 nota de R$ 5");
+                } else {
+                    System.out.println(notas5 + " notas de R$ 5");
+                }
             }
-        }
 
-        if (notas1 > 0) {
-            if (notas1 == 1) {    
-                System.out.println("1 nota de R$ 1");
-            } else {
-                System.out.println(notas1 + " notas de R$ 1");
+            if (notas1 > 0) {
+                if (notas1 == 1) {    
+                    System.out.println("1 nota de R$ 1");
+                } else {
+                    System.out.println(notas1 + " notas de R$ 1");
+                }
             }
         }
     }
